f54ee4e96c60d5dccbd862e462171418023f0567
Manikandan13102003 <146917439+Manikandan13102003@users.noreply.github.com>
2025-02-27 13:23:50 +0530

    C5.jsx

---
+++ 27-02-25/Task2/C5.jsx
@@ -0,0 +1,12 @@
+// outside-src/C5.jsx
+import React from 'react';
+
+const C5 = () => {
+  return (
+    <div>
+      <h6>Component C5 (Dynamically Loaded)</h6>
+    </div>
+  );
+};
+
+export default C5;
