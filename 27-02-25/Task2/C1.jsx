3ce176e39909efeb959290d28ab0a5f512ddbab8
Manikandan13102003 <146917439+Manikandan13102003@users.noreply.github.com>
2025-02-27 13:21:35 +0530

    C1.jsx

---
+++ 27-02-25/Task2/C1.jsx
@@ -0,0 +1,14 @@
+// src/C1.jsx
+import React from 'react';
+import C2 from './f1/C2';
+
+const C1 = () => {
+  return (
+    <div>
+      <h2>Component C1</h2>
+      <C2 />
+    </div>
+  );
+};
+
+export default C1;
