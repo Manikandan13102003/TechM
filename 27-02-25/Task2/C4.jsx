1490f014bcbeae77b49bcc47a884e33baaa3ec9d
Manikandan13102003 <146917439+Manikandan13102003@users.noreply.github.com>
2025-02-27 13:23:19 +0530

    C4.jsx

---
+++ 27-02-25/Task2/C4.jsx
@@ -0,0 +1,12 @@
+// outside-src/C4.jsx
+import React from 'react';
+
+const C4 = () => {
+  return (
+    <div>
+      <h5>Component C4 (Dynamically Loaded)</h5>
+    </div>
+  );
+};
+
+export default C4;
