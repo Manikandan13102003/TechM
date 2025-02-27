610c380ba9170d6f4f0021627b06b97a5b75e985
Manikandan13102003 <146917439+Manikandan13102003@users.noreply.github.com>
2025-02-27 13:24:23 +0530

    Main.jsx

---
+++ 27-02-25/Task2/Main.jsx
@@ -0,0 +1,14 @@
+// src/Main.jsx
+import React from 'react';
+import C1 from './C1';
+
+const Main = () => {
+  return (
+    <div>
+      <h1>Main Component</h1>
+      <C1 />
+    </div>
+  );
+};
+
+export default Main;
